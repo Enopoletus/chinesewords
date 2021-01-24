8f7c10f1bd199da024e6520b534d658bbb45f897
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-23 21:27:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function readTextFile() {
 const xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-  if (xmlhttp.status == 200) {thetext.push(xmlhttp.responseText); console.log(thetext)}
+  if (xmlhttp.status == 200) {thetext.push(xmlhttp.responseText)}
     else if (xmlhttp.status == 400) {console.log('There was an error 400');}
     else {console.log('something else other than 200 was returned');};
   };
 xmlhttp.open("GET", chinesewords.txt, true);
 xmlhttp.send();
+console.log(thetext);
 }}
