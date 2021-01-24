66d53f841cd82b90263937bf693c5b2687efbee6
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-24 15:35:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -89,12 +89,10 @@ function postweb(){
 }
 function postenter(){
   postweb();
-  document.getElementById("textbox").value = "";
   numright = numright+1;
   console.log(numright/(numright+numwrong));
 }
 function postwrong(){
-  document.getElementById("textbox").value = "";
   numwrong = numwrong+1;
   console.log(numright/(numright+numwrong));
 }
