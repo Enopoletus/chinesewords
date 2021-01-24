d447eb33777011e7f4533478fd704a201771139b
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-24 11:29:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ function readTextFile() {
 const xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-    if (xmlhttp.status == 200) {thetext.push(xmlhttp.responseText); postweb()}
-    else if (xmlhttp.status == 400) {console.log('There was an error 400');}
+    if (xmlhttp.status == 200) {thetext.push(xmlhttp.responseText)}
+    else if (xmlhttp.status == 400) {console.log('There was an error 400'); postweb()}
     else {console.log('something else other than 200 was returned');};
   };
 };
   xmlhttp.open("GET", "chinesewords.txt", true);
   xmlhttp.send();
 }
+window.addEventListener("click", checkboxtest);
+window.addEventListener("keyup", checkboxtest);
+function checkboxtest(){
+  if(document.getElementById("inputchars").checked){(document.getElementById("outputchars").remove()};
+  if(document.getElementById("outputchars").checked){(document.getElementById("inputchars").remove()};
+  if(document.getElementById("inputpinyin").checked){(document.getElementById("outputpinyin").remove()};
+  if(document.getElementById("outputpinyin").checked){(document.getElementById("inputpinyin").remove()};
+  }
 function postweb(){
   const lines = thetext[0].split(/\r?\n/).filter(word => word.length > 2);
   const randomline = lines[Math.floor(Math.random() * lines.length)];
