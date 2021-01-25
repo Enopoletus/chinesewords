debf3a324dcd8596ea684fbcebfc7d54aac7363a
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-25 14:10:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function postweb(){
   const lines = thetext[0].split(/\r?\n/).filter(word => word.length > 2);
   const randomline = lines[Math.floor(Math.random() * lines.length)];
   postweb2(document.getElementById("outputchars").checked==true, document.getElementById("outputpinyin").checked == false, document.getElementById("outputdefs").checked==false,
-  randomline.split(";")[1],randomline.split(" ")[0]));
+  randomline.split(";")[1],randomline.split(" ")[0]);
 }
 function postweb2(x1,x2,x3,y1,y2){
   if(x1 && x2 && x3)
