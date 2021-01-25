766dd2d5debb9e87e63da48a11ecc2d60edca571
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-25 14:07:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,20 @@ function postweb(){
   document.getElementById("textbox").value = "";
   const lines = thetext[0].split(/\r?\n/).filter(word => word.length > 2);
   const randomline = lines[Math.floor(Math.random() * lines.length)];
+  postweb2(document.getElementById("outputchars").checked==true, document.getElementById("outputpinyin").checked == false, document.getElementById("outputdefs").checked==false,
+  randomline.split(";")[1],randomline.split(" ")[0]));
+}
+function postweb2(x1,x2,x3,y1,y2){
+  if(x1 && x2 && x3)
+  {document.getElementById("characters").innerHTML = y1;
+   document.getElementById("textbox").addEventListener("keyup", function(event) {
+     if (event.keyCode === 13) {
+      event.preventDefault();
+      if(document.getElementById("textbox").value == y2){postenter()}
+      else{postwrong();};
+     }
+   });
+  };
 }
 function postenter(){
   postweb();
