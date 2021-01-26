1c6f1c09f668df683c967df9d5a675857c42d681
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2021-01-25 19:03:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,98 @@
-window.addEventListener("load", readTextFile);
-const done =[];
-const thetext = [];
-let numright = 0;
-let numwrong = 0;
-function readTextFile() {
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-    if (xmlhttp.status == 200) {thetext.push(xmlhttp.responseText); done.push(1)}
-    else if (xmlhttp.status == 400) {console.log('There was an error 400')}
-    else {console.log('something else other than 200 was returned');};
-  };
-};
-  xmlhttp.open("GET", "chinesewords.txt", true);
-  xmlhttp.send();
-}
-window.addEventListener("load", checkboxtest);
-window.addEventListener("load", evlisttochecks);
-function evlisttochecks(){
-  for(let i of document.getElementsByTagName("input")){if(i.type == "radio" || i.type == "checkbox"){i.addEventListener("click", checkboxtest)}}
-}
-function checkboxtest(){
-  for (let i of document.querySelectorAll("form")[0]){i.style.display = "inline";}
-  for (let i of document.querySelectorAll("form")[1]){i.style.display = "inline";}
-  if(document.getElementById("inputchars").checked){document.getElementById("outputchars").style.display = "none";};
-  if(document.getElementById("outputchars").checked){document.getElementById("inputchars").style.display = "none";};
-  if(document.getElementById("inputpinyin").checked){document.getElementById("outputpinyin").style.display = "none";};
-  if(document.getElementById("outputpinyin").checked){document.getElementById("inputpinyin").style.display = "none";};
-  if (done.length > 0){postweb()};
-  }
-function postweb(){
-  document.getElementById("textbox").value = "";
-  const lines = thetext[0].split(/\r?\n/).filter(word => word.length > 2);
-  const randomline = lines[Math.floor(Math.random() * lines.length)];
-  postweb2(document.getElementById("outputchars").checked, document.getElementById("outputdefs").checked, 0==0,
-  randomline.split(" ")[0]+randomline.split(";")[1],randomline.split("-")[1].split(";")[0]);
-  postweb2(document.getElementById("outputchars").checked, document.getElementById("outputdefs").checked == false, 0==0,
-  randomline.split(" ")[0],randomline.split("-")[1].split(";")[0]);
-  postweb2(document.getElementById("outputpinyin").checked, document.getElementById("outputdefs").checked ==false, 0==0,
-  randomline.split("-")[1].split(" ")[0],randomline.split(" ")[0]);
-  postweb2(document.getElementById("outputpinyin").checked, document.getElementById("outputdefs").checked, 0==0,
-  randomline.split("-")[1],randomline.split(" ")[0]);
-  postweb2(document.getElementById("inputpinyin").checked, document.getElementById("outputchars").checked == false, document.getElementById("outputdefs").checked,
-  randomline.split(";")[1],randomline.split("-")[1].split(";")[0]);
-  postweb2(document.getElementById("inputchars").checked, document.getElementById("outputpinyin").checked == false, document.getElementById("outputdefs").checked,
-  randomline.split(";")[1],randomline.split(" ")[0]);
-}
-function postweb2(x1,x2,x3,y1,y2){
-  if(x1 && x2 && x3)
-  {document.getElementById("characters").innerHTML = y1;
-   document.getElementById("textbox").addEventListener("keyup", function(event) {
-     if (event.keyCode === 13){
-       if(document.getElementById("textbox").value == y2){postenter()}else{postwrong()};
-     };
-     });
-  };
-}
-function postenter(){
-  postweb();
-  numright = numright+1;
-  document.getElementById("scoring").innerHTML = numright.toString()+" right/"+numwrong.toString()+" wrong "+(100*numright/(numright+numwrong)).toString() +"% right";
-  console.log(numright);
-}
-function postwrong(){
-  alert("wrong");
-  numwrong = numwrong+1;
-  document.getElementById("scoring").innerHTML = numright.toString()+" right/"+numwrong.toString()+" wrong "+(100*numright/(numright+numwrong)).toString() +"% right";
-}
+
+    const done = [];
+    const thetext = [];
+    let numright = 0;
+    let numwrong = 0;
+    //REVISED BY "NICK" AT DATA SECRETS LOX
+    let _answer = "";
+
+    window.addEventListener("load", readTextFile);
+    function readTextFile() {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) { thetext.push(xmlhttp.responseText); done.push(1) }
+                else if (xmlhttp.status == 400) { console.log('There was an error 400') }
+                else { console.log('something else other than 200 was returned'); };
+            };
+        };
+        //xmlhttp.open("GET", "chinesewords.txt", true);
+        xmlhttp.open("GET", "https://raw.githubusercontent.com/Enopoletus/chinesewords/gh-pages/chinesewords.txt", true);
+        xmlhttp.send();
+    }
+
+    window.addEventListener("load", evListToChecks);
+
+    function evListToChecks() {
+        for (let i of document.getElementsByTagName("input")) { if (i.type == "radio" || i.type == "checkbox") { i.addEventListener("click", checkboxTest) } }
+    }
+
+    window.addEventListener("load", checkboxTest);
+    function checkboxTest() {
+        for (let el of document.querySelectorAll("form")[0]) { el.style.display = "inline"; }
+        for (let el of document.querySelectorAll("form")[1]) { el.style.display = "inline"; }
+        //this needs wholesale revision
+        if (document.getElementById("inputchars").checked) { document.getElementById("outputchars").style.display = "none"; };
+        if (document.getElementById("outputchars").checked) { document.getElementById("inputchars").style.display = "none"; };
+        if (document.getElementById("inputpinyin").checked) { document.getElementById("outputpinyin").style.display = "none"; };
+        if (document.getElementById("outputpinyin").checked) { document.getElementById("inputpinyin").style.display = "none"; };
+
+        if (done.length > 0) { postweb() };
+    }
+
+    function postweb() {
+        document.getElementById("textbox").value = "";
+        const lines = thetext[0].split(/\r?\n/).filter(word => word.length > 2);
+        const randomline = lines[Math.floor(Math.random() * lines.length)];
+
+        const chars = randomline.split(" ")[0];
+        const pinyin = randomline.split("-")[1].split(";")[0];
+        const pinyinDefs = randomline.split("-")[1];
+        const defs = randomline.split("; ")[1];
+
+        postweb2(document.getElementById("outputchars").checked, document.getElementById("outputdefs").checked, 0 == 0,
+            chars + defs, pinyin);
+        postweb2(document.getElementById("outputchars").checked, document.getElementById("outputdefs").checked == false, 0 == 0,
+            chars, pinyin);
+        postweb2(document.getElementById("outputpinyin").checked, document.getElementById("outputdefs").checked == false, 0 == 0,
+            pinyin, chars);
+        postweb2(document.getElementById("outputpinyin").checked, document.getElementById("outputdefs").checked, 0 == 0,
+            pinyinDefs, chars);
+        postweb2(document.getElementById("inputpinyin").checked, document.getElementById("outputchars").checked == false, document.getElementById("outputdefs").checked,
+            defs, pinyin);
+        postweb2(document.getElementById("inputchars").checked, document.getElementById("outputpinyin").checked == false, document.getElementById("outputdefs").checked,
+            defs, chars);
+    }
+
+    function postweb2(x1, x2, x3, y1, answer) {
+        if (x1 && x2 && x3) {
+            document.getElementById("characters").textContent = y1;
+            _answer = answer;
+        };
+    }
+
+    document.getElementById("textbox").addEventListener("keyup", checkAnswer);
+    function checkAnswer(event) {
+        if (event.keyCode === 13) {
+            if (document.getElementById("textbox").value == _answer) { postenter(); } else { postwrong(); };
+            score();
+        }
+    }
+
+    function postenter() {
+        document.getElementById("result").textContent = "right!";
+        numright += 1;
+        postweb();
+    }
+
+    function postwrong() {
+        document.getElementById("result").textContent = "wrong";
+        numwrong += 1;
+        alert("wrong");
+    }
+
+    function score() {
+        let percentScore = (100 * numright / (numright + numwrong));
+        document.getElementById("scoring").textContent = `${numright} right/${numwrong} wrong ${percentScore}% right`;
+    }
+
